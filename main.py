4ba26a7a0e527ffc8bc1c9735b4e0063dccd25d6
Stryck, Paul <paul.stryck@studium.uni-hamburg.de>
2021-06-11 11:23:19 +0200

    try tpu

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from torchvision import transforms
 from modules import net, trainer
 from modules.dataset import ChestXRayImageDataset, ChestXRayImages
 
+
 transform = transforms.Compose([
     transforms.Resize(224),
     transforms.ToTensor(),
@@ -18,8 +19,6 @@ transform = transforms.Compose([
 ])
 
 def main():
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--data-path', type = str, help = 'Path to training data')
     parser.add_argument('--model-path', type = str, help = 'Path to store models')
@@ -27,7 +26,7 @@ def main():
     parser.add_argument('--val-bs', type = int, default = 64, help = 'val batch size')
     parser.add_argument('--train-bs', type = int, default = 64, help = 'train batch size')
     parser.add_argument('--lr', type = float, default = 1e-5, help = 'Learning Rate passed to optimizer')
-    parser.add_argument('--device', type = str, default = device, help = 'Force usage of device')
+    parser.add_argument('--device', type = str, default = 'cpu', help = 'Force usage of device')
     parser.add_argument('--epochs', type = str, default = 2, help = 'Train for n epochs')
     parser.add_argument('--log-interval', type = int, default = 5, help = 'log every n batches')
     parser.add_argument('--save-interval', type = int, default = 5, help = 'save every n batches')
@@ -36,6 +35,16 @@ def main():
     parser.add_argument('--fold-id', type=int, default=0, help='Which fold id to use for test/val split')
     parser.add_argument('--seed', type=int, default=0, help='Seed the random generator to get reproducability')
     args = parser.parse_args()
+
+    if args.device == 'tpu':
+        import torch_xla
+        import torch_xla.core.xla_model as xm
+        device = xm.xla_device()
+    else if args.device == 'cuda':
+        device = torch.device("cuda")
+    else:
+        device = torch.device('cpu')
+
 
     data_wrapper = ChestXRayImages(args.data_path, folds=args.folds, frac=args.data_frac)
     data_train = ChestXRayImageDataset(
@@ -60,7 +69,7 @@ def main():
     optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),
                            lr = args.lr)
 
-    print('Using device: {}'.format(args.device))
+    print('Using device: {}'.format(device))
     print('With {} Test datasets, {} val data sets and {} train datasets'.format(
         len(data_test), len(data_val), len(data_train)
     ))
